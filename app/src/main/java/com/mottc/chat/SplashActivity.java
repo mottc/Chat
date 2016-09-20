8512ee216d574f2a5b21fd543140be2c1ee29fe0
mottc <mottcode@163.com>
2016-09-20 16:49:11 +0800

    新增开屏页

---
+++ app/src/main/java/com/mottc/chat/SplashActivity.java
@@ -0,0 +1,51 @@
+package com.mottc.chat;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.widget.RelativeLayout;
+
+public class SplashActivity extends Activity {
+
+    private RelativeLayout rootLayout;
+
+    private static final int sleepTime = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        rootLayout = (RelativeLayout) findViewById(R.id.splash_root);
+        AlphaAnimation animation = new AlphaAnimation(0.3f, 1.0f);
+        animation.setDuration(sleepTime);
+
+        rootLayout.startAnimation(animation);
+    }
+
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+
+        new Thread(new Runnable() {
+            public void run() {
+
+                try {
+                    Thread.sleep(sleepTime);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+
+                //进入主页面
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+
+            }
+        }).start();
+    }
+
+}
