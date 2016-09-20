e433523667b6758bb05566efd2d1afcbb1616763
mottc <mottcode@163.com>
2016-09-20 17:16:01 +0800

    新增登陆、注册界面

---
+++ app/src/main/java/com/mottc/chat/LoginActivity.java
@@ -0,0 +1,82 @@
+package com.mottc.chat;
+
+import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import shem.com.materiallogin.DefaultLoginView;
+import shem.com.materiallogin.DefaultRegisterView;
+import shem.com.materiallogin.MaterialLoginView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        final MaterialLoginView login = (MaterialLoginView) findViewById(R.id.login);
+        ((DefaultLoginView) login.getLoginView()).setListener(new DefaultLoginView.DefaultLoginViewListener() {
+            @Override
+            public void onLogin(TextInputLayout loginUser, TextInputLayout loginPass) {
+                //Handle login
+                String user = loginUser.getEditText().getText().toString();
+                if (user.isEmpty()) {
+                    loginUser.setError("User name can't be empty");
+                    return;
+                }
+                loginUser.setError("");
+
+                String pass = loginPass.getEditText().getText().toString();
+                if (!pass.equals(user)) {
+                    loginPass.setError("Wrong password");
+                    return;
+                }
+                loginPass.setError("");
+
+                Toast.makeText(LoginActivity.this,"Login success!",Toast.LENGTH_LONG).show();
+
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+
+
+                //Snackbar.make(login, "Login success!", Snackbar.LENGTH_LONG).show();
+            }
+        });
+
+        ((DefaultRegisterView) login.getRegisterView()).setListener(new DefaultRegisterView.DefaultRegisterViewListener() {
+            @Override
+            public void onRegister(TextInputLayout registerUser, TextInputLayout registerPass, TextInputLayout registerPassRep) {
+                //Handle register
+                String user = registerUser.getEditText().getText().toString();
+                if (user.isEmpty()) {
+                    registerUser.setError("User name can't be empty");
+                    return;
+                }
+                registerUser.setError("");
+
+                String pass = registerPass.getEditText().getText().toString();
+                if (pass.isEmpty()) {
+                    registerPass.setError("Password can't be empty");
+                    return;
+                }
+                registerPass.setError("");
+
+                String passRep = registerPassRep.getEditText().getText().toString();
+                if (!pass.equals(passRep)) {
+                    registerPassRep.setError("Passwords are different");
+                    return;
+                }
+                registerPassRep.setError("");
+
+                Toast.makeText(LoginActivity.this,"Register success!",Toast.LENGTH_LONG).show();
+
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+
+
+                //Snackbar.make(login, "Register success!", Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+}
