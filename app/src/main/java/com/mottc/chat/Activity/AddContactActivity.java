227d490e557f6270a9120e2f025fd93f292add33
mottc <mottcode@163.com>
2016-10-09 09:48:49 +0800

    新增“添加好友”

---
+++ app/src/main/java/com/mottc/chat/Activity/AddContactActivity.java
@@ -0,0 +1,87 @@
+package com.mottc.chat.Activity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.hyphenate.chat.EMClient;
+import com.mottc.chat.R;
+
+public class AddContactActivity extends AppCompatActivity {
+
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_contact);
+
+        final EditText et_username = (EditText) this.findViewById(R.id.et_username);
+        final EditText et_reason = (EditText) this.findViewById(R.id.et_reason);
+
+        Button btn_add = (Button) this.findViewById(R.id.btn_add);
+        btn_add.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String username = et_username.getText().toString().trim();
+                String reason = et_reason.getText().toString().trim();
+
+                if (TextUtils.isEmpty(username)) {
+
+                    Toast.makeText(getApplicationContext(), "请输入用户名", Toast.LENGTH_SHORT).show();
+                    return;
+
+                }
+                addContact(username,reason);
+            }
+
+        });
+    }
+    /**
+     * 添加contact
+     *
+     * @param view
+     */
+    public void addContact(final String username,final String reason) {
+        progressDialog = new ProgressDialog(this);
+        String stri = getResources().getString(R.string.Is_sending_a_request);
+        progressDialog.setMessage(stri);
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+
+        new Thread(new Runnable() {
+            public void run() {
+
+                try {
+
+                    EMClient.getInstance().contactManager().addContact(username, reason);
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            progressDialog.dismiss();
+                            String s1 = getResources().getString(R.string.send_successful);
+                            Toast.makeText(getApplicationContext(), s1, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } catch (final Exception e) {
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            progressDialog.dismiss();
+                            String s2 = getResources().getString(R.string.Request_add_buddy_failure);
+                            Toast.makeText(getApplicationContext(), s2 + e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+
+    public void back(View v) {
+        finish();
+    }
+}
