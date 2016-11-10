38b832897c56dc4ca0c6e88861fb78923635dffb
mottc <mottcode@163.com>
2016-11-10 21:33:47 +0800

    群组详情页

--- app/src/main/java/com/mottc/chat/Activity/GroupDetailActivity.java
+++ app/src/main/java/com/mottc/chat/Activity/GroupDetailActivity.java
@@ -2,6 +2,7 @@ package com.mottc.chat.Activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -10,8 +11,10 @@ import com.mottc.chat.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class GroupDetailActivity extends AppCompatActivity {
+
 
     @BindView(R.id.back)
     ImageButton mBack;
@@ -25,5 +28,20 @@ public class GroupDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_detail);
         ButterKnife.bind(this);
+
+
+    }
+
+
+    @OnClick({R.id.back, R.id.detail_group_avatar, R.id.detail_group_name})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.back:
+                break;
+            case R.id.detail_group_avatar:
+                break;
+            case R.id.detail_group_name:
+                break;
+        }
     }
 }
