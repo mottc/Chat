75183da4dcaeb5b3e2569111852881b3e081f971
mottc <mottcode@163.com>
2016-11-22 15:35:48 +0800

    群组加好友

--- app/src/main/java/com/mottc/chat/Activity/GroupDetailActivity.java
+++ app/src/main/java/com/mottc/chat/Activity/GroupDetailActivity.java
@@ -60,7 +60,6 @@ public class GroupDetailActivity extends AppCompatActivity {
             @Override
             public void OnGroupMembersListClick(String item) {
                 startActivity(new Intent(GroupDetailActivity.this, UserDetailActivity.class).putExtra("username", item).putExtra("isNew",true));
-
             }
         });
     }
