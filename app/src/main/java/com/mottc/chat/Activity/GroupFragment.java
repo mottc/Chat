3b15894644c73f7bae13dc1d288fa1acd9abd544
mottc <mottcode@163.com>
2016-11-24 14:16:52 +0800

    修改图标

--- app/src/main/java/com/mottc/chat/Activity/GroupFragment.java
+++ app/src/main/java/com/mottc/chat/Activity/GroupFragment.java
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -71,7 +70,6 @@ public class GroupFragment extends Fragment {
                 mMyGroupRecyclerViewAdapter.notifyDataSetChanged();
             }
         });
-        Log.i("GroupFragment", "onResume: " + "!");
     }
 
     @Override
