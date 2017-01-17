41d5a7e602e2828a20d05847722c9539d7c6d4e5
mottc <mottcode@163.com>
2017-01-17 16:12:40 +0800

    Chat1.x版本停止开发

--- app/src/main/java/com/mottc/chat/Activity/Adapter/MyGroupRecyclerViewAdapter.java
+++ app/src/main/java/com/mottc/chat/Activity/Adapter/MyGroupRecyclerViewAdapter.java
@@ -26,7 +26,6 @@ public class MyGroupRecyclerViewAdapter extends RecyclerView.Adapter<MyGroupRecy
         mValues = items;
         mListener = listener;
         mContext = context;
-
     }
 
     @Override
